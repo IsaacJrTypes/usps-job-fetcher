cd79a2c1fdfd5cb8e107ae25592ee58ae7ff3d25
Isaac J <jimenez.isaac.jr@gmail.com>
2025-10-23 21:56:49 -0700

    Add navigation to job result page

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ import { jobPortalURL } from './constants.js';
 
 // Launch the browser and open a new blank page.
 const browser = await puppeteer.launch({
-    headless: false,  
-    defaultViewport: null, 
+    headless: false,
+    defaultViewport: null,
     args: ['--start-maximized']
-    });
+});
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
-const pagePromise = await page.goto(jobPortalURL);
+const pagePromise = await page.goto(jobPortalURL, {
+    waitUntil: 'networkidle2',
+    timeout: 60000
+});
 
-if(pagePromise) {
+if (pagePromise) {
     console.log('Status:', pagePromise.status());
     console.log('URL:', pagePromise.url());
     console.log('Headers:', pagePromise.headers());
+
+    // Get job by div id, select
+    await page.waitForSelector('#WD9C');
+    await page.$('#WD9C').then(el => el.click());
+
+    // Search jobs
+    await page.waitForSelector('#WDB0');
+    await page.$('#WDB0').then(el => el.click());
+
 } else {
     console.log('Page failed to load');
 }
