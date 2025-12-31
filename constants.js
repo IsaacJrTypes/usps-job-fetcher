27cd3ec16a9af2e52b014903c5cfcef45d34aef3
Isaac J <jimenez.isaac.jr@gmail.com>
2025-12-30 22:32:14 -0800

    Update .gitignore file

---
+++ constants.js
@@ -0,0 +1,16 @@
+export const jobPortalURL = "https://wp1-ext.usps.gov/sap/bc/webdynpro/sap/hrrcf_a_unreg_job_search?_gl=1*bkr5wx*_gcl_au*MTI4NDUxNzg0MC4xNzU3NDMwMjI0*_ga*MTgxNDc0NTMwNS4xNzU3NDMwMjI0*_ga_3NXP3C8S9V*czE3NTk3NzUwNjUkbzEyJGcwJHQxNzU5Nzc1MDY4JGo1NyRsMCRoMA..#...";
+
+export const jobDivIds = {
+    'it-jobs': {
+        jobTitleId: '#WD9C',
+        jobTableId: '#WDEA',
+        jobResultId: '#WDEB',
+        download: '#save'
+        
+    },
+    'delivery-jobs': {
+        jobTitleId: '#WD96',
+        jobResultId: '#WDEB',
+    }
+
+};
